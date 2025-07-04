280af9726df8af084b7b7d213a000f6e74cc0b60
soosanna <soosanna39@gmail.com>
2025-07-04 11:22:45 +0530

    added lamda url

--- script.js
+++ script.js
@@ -3,8 +3,13 @@ async function getCounter() {
         const response = await fetch('https://spfnfkfcgbtbqxrcej5rekalyq0topav.lambda-url.ap-south-1.on.aws/', {
             method: 'GET'
         });
-        const data = await response.json();
-        console.log(data);
+
+        const result = await response.json(); // This will give { statusCode, body }
+        console.log('Lambda Full Response:', result);
+
+        const data = JSON.parse(result.body); // Parse the string inside 'body'
+        console.log('Parsed Data:', data);
+
         document.getElementById('counter').innerText = `Views: ${data.views}`;
     } catch (error) {
         console.error('Error fetching data:', error);
